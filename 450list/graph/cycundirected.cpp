344ef71ff7b51f48baaa3c84d8cd4442ddebf8ec
Omkar Ingale <52862034+Im-OMkar@users.noreply.github.com>
2021-06-15 22:23:43 +0530

    Create cycundirected.cpp

---
+++ 450list/graph/cycundirected.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{   public:
+
+    bool checkcyc(int s, vector<int> &visited, vector<int> adj[], int V){
+
+        queue<pair<int, int>>q;
+        visited[s]=true;
+        q.push({s,-1});
+        
+        while(!q.empty()){
+            int node= q.front().first;
+            int par= q.front().second;
+            q.pop();
+            
+            for(auto it: adj[node]){
+                if(!visited[it]){
+                    visited[it]= true;
+                    q.push({it, node});
+                }
+                else if(par!= it){
+                    return true;
+                }
+            }
+           
+        }
+         return false;
+    }
+    public:
+    //Function to detect cycle in an undirected graph.
+	bool isCycle(int V, vector<int>adj[])
+	{
+	    vector<int> vis(V, 0);
+	    for(int i=0; i<V; i++){
+	        if(!vis[i]){
+	            if(checkcyc(i, vis, adj, V))return true;
+	        }
+	    }
+	    return false;
+	}
+};
