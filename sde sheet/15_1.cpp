1b855eabafc51e960635e7474e4b47ef53834c69
Omkar Ingale <52862034+Im-OMkar@users.noreply.github.com>
2021-06-25 14:15:53 +0530

    Create 15_1.cpp

---
+++ sde sheet/15_1.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        vector<string>temp;
+        string str="";
+        
+        for(int i=0; i<S.length(); i++){
+            if(S[i]=='.') {
+                temp.push_back(str);
+                str="";
+            }
+            
+            else{
+                str+=S[i];
+            }
+        }
+        temp.push_back(str);
+        string ans;
+        for(int i=temp.size()-1; i>0; i--){
+            ans=ans+temp[i]+".";
+        }
+        ans+=temp[0];
+        return ans;
+    } 
+};
